2a6b695dcc5a7b3e43d4111a09f2d59deb063a2a
Kozata <xydalxyz@gmail.com>
2026-05-31 12:55:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def run_app():
             print("Exiting calculator. Goodbye!")
             break
             
-        if choice != ['1', '2', '3', '4', '5', '6']:
+        if choice not in ['1', '2', '3', '4', '5', '6']:
             print("Invalid selection. Please try again.")
             continue
             
