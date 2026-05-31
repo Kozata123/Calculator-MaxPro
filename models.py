57f3a661cddb5fa399810ce7047d1709f16311ae
Kozata <xydalxyz@gmail.com>
2026-05-31 12:59:14 +0300

    Update models.py
    Added rounding to the 5th digit.

--- models.py
+++ models.py
@@ -4,24 +4,24 @@ class Calculator:
     def __init__(self):
         self.name = "Calculator ProMax LuxeDeluxe"
 
-    def add(self, num1, num2):
-        return num1 + num2
+   def add(self, num1, num2):
+        return round(num1 + num2,ndigits=5)
 
     def subtract(self, num1, num2):
-        return num1 - num2
+        return round(num1 - num2,ndigits=5)
 
     def multiply(self, num1, num2):
-        return num1 * num2
+        return round(num1 * num2,ndigits=5)
 
     def divide(self, num1, num2):
         if num2 == 0:
             return "Error: Division by zero is undefined."
-        return num1 / num2
+        return round(num1 / num2,ndigits=5)
 
     def power(self, num1, num2):
-        return num1 ** num2
+        return round(num1 ** num2,ndigits=5)
 
     def square_root(self, num):
         if num < 0:
             return "Error: Cannot take the square root of a negative number."
-        return math.sqrt(num)
+        return round(math.sqrt(num),ndigits=5)
